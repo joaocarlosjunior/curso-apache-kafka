3f8cd3b248d864a6a8e735e9a008fd5e86f9543e
João Carlos Júnior <joaocarlos.cic1@gmail.com>
2026-03-25 17:56:25 -0300

    feat: adiciona consumer EmailNotification

---
+++ EmailNotificationMicroservice/src/test/java/com/joaocarlos/emailNotification/EmailNotificationMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaocarlos.emailNotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailNotificationMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
