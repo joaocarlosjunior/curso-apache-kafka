320478738bc44ec73cf2c18a819dc756c9709423
João Carlos Júnior <joaocarlos.cic1@gmail.com>
2026-04-01 17:00:32 -0300

    feat: adiciona endpoint no mock service para exemplos

--- MockService/src/main/java/com/joaocarlos/mockService/controller/MockController.java
+++ MockService/src/main/java/com/joaocarlos/mockService/controller/MockController.java
@@ -1,4 +1,19 @@
 package com.joaocarlos.mockService.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/response")
 public class MockController {
+
+    @GetMapping("/200")
+    public ResponseEntity<String> response200String() {
+        return ResponseEntity.ok().body("200");
+    }
+
+    @GetMapping("/500")
+    public ResponseEntity<String> response500String() {
+        return ResponseEntity.internalServerError().build();
+    }
 }
