9336b18ee20783ad842829751232f9757fb8e340
João Carlos Júnior <joaocarlos.cic1@gmail.com>
2026-04-10 10:20:12 -0300

    feat: adiciona servicos Transfer, Withdrwal e Deposit

--- MockService/src/main/java/com/joaocarlos/mockService/controller/MockController.java
+++ MockService/src/main/java/com/joaocarlos/mockService/controller/MockController.java
@@ -2,9 +2,11 @@ package com.joaocarlos.mockService.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/response")
+@RestController
+@RequestMapping("/response")
 public class MockController {
 
     @GetMapping("/200")
